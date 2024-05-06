43d780e0f15b95191d6817e96f2f7f3fcc69f08e
Jamie Gaehring <gaehj457@gmail.com>
2024-05-06 01:23:34 -0400

    Minimize BoardView layout & style.

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -2,98 +2,30 @@
 </script>
 
 <template>
-  <div id="the-board-wrapper">
-    <div id="the-board">
-      <header>
-        <div class="wrapper">
-          <h1>Farm Flow</h1>
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/demo">Demo</RouterLink>
-            <RouterLink to="/demo/about">About</RouterLink>
-          </nav>
-        </div>
-      </header>
-      <main>
-        <h2>The Board</h2>
-      </main>
-    </div>
-  </div>
+  <Teleport to="body">
+    <header>
+      <h1>Farm Flow</h1>
+    </header>
+    <main>
+      <section>
+        <h1>The Board</h1>
+      </section>
+    </main>
+  </Teleport>
 </template>
 
 <style scoped>
-#the-board-wrapper {
-  min-height: 100vh;
-}
-
-#the-board {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-  font-weight: normal;
-}
-
 header {
   line-height: 1.5;
-  max-height: 100vh;
+  height: 3rem;
+  padding: 1.5rem 1.5rem 0  1.5rem;
+}
+main {
+  display: flex;
+  place-items: center;
+  max-width: 1280px;
+  min-height: calc(100vh - 3rem);
+  margin: auto;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  #the-board-wrapper {
-    display: flex;
-    place-items: center;
-  }
-
-  #the-board {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
