186fe9022b1c16992086612a5353fe45b3d4ccb2
Jamie Gaehring <gaehj457@gmail.com>
2024-05-05 23:13:21 -0400

    Relegate demo to separate routes & directories.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WelcomeView from '@/demo/views/WelcomeView.vue'
+import DemoView from '@/demo/views/DemoView.vue'
+import BoardView from '@/views/BoardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +9,28 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: BoardView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/demo',
+      name: 'demo',
+      component: DemoView,
+      children: [
+        {
+          path: '',
+          name: 'welcome',
+          component: WelcomeView,
+        },
+        {
+          path: 'about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/demo/views/AboutView.vue')
+        }
+      ],
+    },
   ]
 })
 
