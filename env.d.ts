3f7304c3570213f2be6dbf8d1f499ee4d6172f5b
Jamie Gaehring <gaehj457@gmail.com>
2024-05-20 16:46:30 -0400

    Install & config vite-svg-loader.

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// Allow vite-svg-loader and other Vite plugins to access static assets, such as
+// SVG files, as Vue components.
+declare module "*?component" {
+  const content: string;
+  export default content;
+}
