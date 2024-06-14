75c34213f60d5bfb644ad3cafb55e4ac7fc48789
Jamie Gaehring <gaehj457@gmail.com>
2024-06-14 11:05:45 -0400

    Provide transform for optional identifiers.

--- src/utils/idfier.ts
+++ src/utils/idfier.ts
@@ -1,3 +1,15 @@
-import { pick } from 'ramda';
+import { validate } from 'uuid';
+import { always, both, ifElse, pick, propIs, propSatisfies } from 'ramda';
+
+export const isIdfier = both(
+  propSatisfies(validate, 'id'),
+  propIs(String, 'type'),
+);
 
 export const toIdfier = pick(['id', 'type']);
+
+export const toOptionalIdfier = ifElse(
+  isIdfier,
+  toIdfier,
+  always(null),
+);
