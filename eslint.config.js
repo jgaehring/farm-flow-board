97ee3fec99d2b7e6f59752eef4e7062d2652288c
Jamie Gaehring <gaehj457@gmail.com>
2024-11-29 11:49:46 -0500

    Upgrade to eslint 9.0 w/ compat vue/tsc tooling.

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: [
+      '**/dist/**',
+      '**/dist-ssr/**',
+      '**/coverage/**',
+      './*.config.*',
+    ],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+]
