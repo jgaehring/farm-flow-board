5b97d42a0668da73db46474fe6bcc38b03ad8f9c
Jamie Gaehring <jamie@runrig.org>
2025-01-27 15:36:44 -0500

    Setup Runrig ESLint rules for Vite, Vue, TS.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { fileURLToPath } from 'node:url';
 import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVitest from '@vitest/eslint-plugin'
+import runrigStyle from '@runrig/eslint-config-runrig';
 
-export default [
+const settings = {
+  'import/resolver': {
+    typescript: true,
+    vite: {
+      viteConfig: {
+        resolve: {
+          alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+          },
+        },
+      },
+    },
+  },
+};
+
+export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    settings,
   },
 
   {
@@ -18,11 +37,42 @@ export default [
     ],
   },
 
+  ...runrigStyle,
   ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
+  vueTsConfigs.recommended,
+
+  {
+    name: 'app/vue-script-setup-ts',
+    files: ['**/*.vue'],
+    settings,
+    /**
+     * VUE SFC WORKAROUNDS
+     * These are all rules that I'm just turning off for now out of convenience
+     * b/c there's some issue between Vue, TS, and ESLint when using SFCs that
+     * have a <script setup lang="ts"> template, but I don't have time to fiddle
+     * around with it right now.
+     */
+    rules: {
+      // https://eslint.vuejs.org/user-guide/#the-variables-used-in-the-template-are-warned-by-no-unused-vars-rule
+      '@typescript-eslint/no-unused-vars': 'off',
+      // https://eslint.vuejs.org/rules/valid-v-for.html
+      'vue/valid-v-for': ['off'],
+      'import/no-unresolved': ['error', {
+        ignore: [
+          // For vite-svg-loader: https://github.com/jpkleemans/vite-svg-loader
+          // This should be fixable with an alias plus rollup's find/replace,
+          // but that hasn't worked so far.
+          '\\.svg\\?component',
+          // Issue w/ `"exports"` in package.json, related to but not fixed by:
+          // https://github.com/unovue/radix-vue/issues/925
+          'radix-vue/namespaced',
+        ],
+      }],
+    },
+  },
 
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-]
+);
